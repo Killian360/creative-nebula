a49cd40e935a0552e99fd03eb6e3f70dcf907335
Killian360 <33862819+Killian360@users.noreply.github.com>
2018-05-10 14:44:42 +0200

    update gitignore

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 
-import mailer from './mailer'
+import mailer from "./mailer"
 
 const app = express()
 
 // serve the react app files
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -15,7 +21,7 @@ app.get('*', (req, res) => {
 })
 
 app.post('/api/contact', (req, res) => {
-  const { email = "", subject = "", message = "" } = req.body
+  const { email = '', subject = '', message = '' } = req.body
   mailer({ email, subject, text: message }).then(() => {
     return res.json({"validate": "true"});
   }).catch((error) => {
